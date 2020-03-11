12a21d213f8dde2ce43a33c7ece8b77f36e90197
Glitch (chess-uvsq) <none>
2020-03-11 21:25:40 +0000

    documentation et commentaire du code

--- plateau.js
+++ plateau.js
@@ -1,111 +1,32 @@
-var tailleCase = 70;
+var width;
+var height;
+var squareSize;
+var pion;
+var pion2;
+window.addEventListener("resize", resize);
 var nombreCases = 8;
 
-class Position2D {
-  constructor(x,y) {
-    if(x == undefined) this.x = 0;
-    else this.x = x;
-    if(y == undefined) this.y = 0;
-    else this.y = y;
-    
-  }
+
+//Initialisation des variables width and height au début et à chaque resize de la fenêtre d'affichage
+function initVal(){
+  width = window.innerWidth;//Récupération de la largeur de la fenêtre
+  height = window.innerHeight;///Récupération de la hauteur de la fenêtre
   
-  log(){
-    return '(' + this.x + ',' + this.y + ')';
+  if (window.innerHeight > window.innerWidth) {//Si l'orientation de la fenêtre est portrait
+    squareSize = (0.6 * width) / 8;//La longueur de la case est liée à width
   }
-  
-  move(x, y){
-    this.x += x;
-    this.y += y;
-  }
-  
-  set(x,y){
-    this.x = x;
-    this.y = y;
-  }
+  else {squareSize = (0.6 * height) / 8;}//La largeur de la case est liée à height
 }
 
-class Pion {
-  constructor(x,y) {
-    this.position = new Position2D(x,y);
-    this.src = "https://library.kissclipart.com/20181007/wq/kissclipart-pawn-chess-sprite-clipart-chess-piece-pawn-fb6e2722e253d586.jpg";
-    console.log("nouveau pion en " + this.position.log());
-  }
-  
-  //place le pion sur le plateau
-  affiche(){
-    let td = this.getCase();
-    let piece = document.createElement('img');
-    td.appendChild(piece);
-    let me = this; //se copier pour les fonctions ou le this n'est plus possible
-    piece.src = this.src;
-    piece.width = tailleCase/2;
-    piece.height = tailleCase/2;
-    piece.style.margin = '' + tailleCase/4 + 'px ' + tailleCase/4 + 'px';
-    this.setSelectionnable();
-  }
-  
-  //rendre le pion sélectionnable
-  setSelectionnable(){
-    let me = this;
-    this.getImg().addEventListener('click', function(e){
-      me.selectNewCase();
-    })
-  }
-  
-  setNonSelectionnable(){
-    this.getImg().removeEventListener('click');
-  }
-  
-  //return la case <td> associé au pion
-  getCase(){
-    return document.getElementById('Case' + this.position.x + this.position.y);
-  }
-  
-  //return l'image <img associé au pion>
-  getImg(){
-    return this.getCase().firstElementChild;
-  }
-  
-  //deplace le pion su la case indiqué
-  move(x,y){
-    if(x != this.position.x || y != this.position.y) {
-      let td = document.getElementById('Case' + this.position.x + this.position.y);
-      let child = td.removeChild(td.firstChild);
-      this.position.set(x,y);
-      td = document.getElementById('Case' + this.position.x + this.position.y);
-      td.appendChild(child);
-    }
-  }
-  
-  //rendre les cases disponibles a la selection
-  selectNewCase(){
-    let td;
-    for(let y = 0; y < nombreCases; y++){
-      for(let x = 0; x < nombreCases; x++){
-        td = document.getElementById('Case' + x + y);
-        
-        if(!(this.position.x == x && this.position.y == y)){ //rendre toutes les cases selectionnable sauf celle du pion
-          setCaseSelectionnable(td,this);
-        }
-        
-        else { //actions sur la case du pion
-          //setCaseSelectionnable(td,this);
-        }
-      }
-    }
-  }
-}
 
-(function main(){
-  plateauEchec();
-  let pion = new Pion();
-  pion.affiche();
-  pion.getImg();
-})();
-
+/**
+* crée les attributs du tableau pour faire l eplateau de jeu
+* chaque case aura un identifiant associé à ses coordonnées qui prends la forme "Casexy" x et y les valeurs de la position
+* une classe whiteCase ou blackCase sera affectée à la case selon qu'elle soit blanche ou noir
+*/
 function plateauEchec(){
   let plateau = document.getElementById('plateau');
+  initVal();
   let tr;
   let td;
   
@@ -115,16 +36,14 @@ function plateauEchec(){
   table.setAttribute('id', 'tableEchec');
   
   for(let y = 0; y < nombreCases; y++){
-    
     tr = document.createElement('tr');
     tr.setAttribute('id','row' + y);
     
     for(let x = 0; x < nombreCases; x++){
-      
       td = document.createElement('td');
       td.setAttribute('id', 'Case' + x + y);
-      td.setAttribute('width', tailleCase + 'px');
-      td.setAttribute('height', tailleCase + 'px');
+      /*td.setAttribute('width', squareSize + 'px');
+      td.setAttribute('height', squareSize + 'px');*/
       if((x + y) % 2 == 0){
         td.setAttribute('class', 'whiteCase');
       }
@@ -138,35 +57,214 @@ function plateauEchec(){
 }
 
 
-//la case td du tableau donné en parametre devient disponible pour le placement du pion
+/**
+* classe représentant une position sur un plan 2D (x,y)
+*/
+class Position2D {
+  
+  /**
+  * affecte les valeurs x et y aux coordonnées du points.
+  * dans le cas ou les valeurs ne sont pas données, la valeur sera affectée par défaut
+  */
+  constructor(x,y) {
+    if(x == undefined) this.x = 0;
+    else this.x = x;
+    if(y == undefined) this.y = 0;
+    else this.y = y;
+    
+  }
+  
+  //retourne une chaine de caractère représentant la position (pour afficher dans la console)
+  log(){
+    return '(' + this.x + ',' + this.y + ')';
+  }
+  
+  // définir une nouvelle position
+  set(x,y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+/**
+* classe représentant un pion basique, sert de structure pour les pions du jeu
+*/
+class Pion {
+  
+  //construit le pion
+  constructor(x,y) {
+    this.position = new Position2D(x,y);
+    this.src = "https://cdn.glitch.com/cc360787-9153-4d34-a2e2-ddbd9bc2b9e4%2FBlackPawn.png?v=1583876671281";
+    console.log("nouveau pion en " + this.position.log());
+  }
+  
+  /*
+  * place le pion sur le plateau en ajoutant la photo du pion associé à l'instance du pion
+  * active l'évènement de sélection (voir doc "setSelectable()")
+  */
+  affiche(){
+    let td = this.getCase();
+    let piece = document.createElement('img');
+    td.appendChild(piece);
+    piece.src = this.src;
+    piece.width = squareSize;
+    piece.height = squareSize;
+    //piece.style.margin = '' + tailleCase/4 + 'px ' + tailleCase/4 + 'px';
+    this.setSelectable();
+  }
+  
+  //activer l'évènement permettant à l'utilisateur de le choisir pour le déplacer
+  setSelectable(){
+    let me = this; //se copier pour les fonctions ou le this n'est plus possible
+    let img = this.getImg();
+    if(img != undefined) img.addEventListener('click', function(e){
+      me.selectNewCase();
+    });
+  }
+  
+  //retire l'évènement permettant de le déplacer
+  setNonSelectable(){
+    let img = this.getImg();
+    if(img != undefined) img.removeEventListener('click');
+  }
+  
+  //return la case <td> associé au pion
+  getCase(){
+    return document.getElementById('Case' + this.position.x + this.position.y);
+  }
+  
+  //return l'image <img> associé au pion
+  getImg(){
+    return this.getCase().lastChild;
+  }
+  
+  /*
+  * deplace le pion su la case indiqué en retirant l'image de l'ancienne case et l'ajoute à la nouvelle
+  * si une image est déjà présente dans la case de destination, alors il y a collision
+  */
+  move(x,y){
+    if(x != this.position.x || y != this.position.y) {
+      let td = this.getCase();
+      let child = td.removeChild(td.lastChild);
+      this.position.set(x,y);
+      td = this.getCase();
+      if(td.lastChild != null) td.removeChild(td.lastChild);
+      td.appendChild(child);
+    }
+    
+  }
+  
+  //Efface le pion de sa position, retire son image du plateau
+  delete(){
+    let td = this.getCase();
+    td.removeChild(td.lastChild);
+  }
+  
+  /*
+  * modifie les cases qui respectent les règles de déplacement du pion
+  * sélectionne certaines cases et les modifie pour qu'elles soient accessibles au pion
+  * se référer à la doc de "setCaseSelectionnable()" qui rend une case accessible au pion
+  */
+  selectNewCase(){
+    let td;
+    for(let y = 0; y < nombreCases; y++){
+      for(let x = 0; x < nombreCases; x++){
+        td = document.getElementById('Case' + x + y);
+        
+        if(!(this.position.x == x && this.position.y == y)){ //rendre toutes les cases selectionnable sauf celle du pion
+          setCaseSelectionnable(td,this);
+        }
+        
+        else { //actions sur la case du pion (fonctionne pas -_-)
+          let img = this.getImg();
+          img.onmouseclick = function(e){
+            console.log("annule deplacement");
+            resetAllCases();
+            this.setSelectable();
+          }
+        }
+      }
+    }
+  }
+}
+
+/*Fonction appelée à chaque redimensionnement, pour adapter la taille de l'interface du jeu à la 
+fenêtre d'affichage*/
+function resize(){
+  initVal();
+  
+  pion.delete();
+  pion.affiche();
+  pion2.delete();
+  pion2.affiche();
+}
+
+//fonction de départ du script
+(function main(){
+  plateauEchec();
+  pion = new Pion();
+  pion.affiche();
+  pion2 = new Pion(4,5);
+  pion2.affiche();
+})();
+
+
+
+
+/*
+* la case <td> du tableau donné en parametre devient disponible pour le placement du pion
+* modifie la couleur de la case afin que l'utilisateur puisses la discerner des autres cases
+* ajoute des évènements comme le survolement qui insiste sur le discernement des cases
+* l'évènement de click définit le choix de l'utilisateur pour le déplacement du pion
+* et supprime la modification de couleurs permettant de discerner les cases et supprime les autres évènements
+*/
 function setCaseSelectionnable(td,pion){
-  td.onmouseenter = function(e){ //la case devient bleu si elle est survolée
-    e.target.style.backgroundColor = 'cyan';
+  //couleur de discernement de la case pour être choisit pour le déplacement (rouge si un pion se trouve déjà sur la case)
+  var color = (td.firstChild == undefined)? 'green' : 'red';
+  
+  td.onmouseenter = function(e){ //la case devient coloré différement si elle est survolée
+    e.target.style.backgroundColor = color;
+    
   }
   td.onmouseout = function(e){ //la case redevient de sa couleur d'origine
     if(e.target.getAttribute('class') == 'whiteCase'){
       e.target.style.backgroundColor = 'white';
     }
     else {
-      e.target.style.backgroundColor = 'maroon';
+      e.target.style.backgroundColor = 'black';
     }
   }
-  td.style.color = 'cyan';
+  td.style.color = color; //ajoute un contour pour indiquer que la case peut être choisit
   
   td.onclick = function(e){ //si l'on sélectionne la case
     console.log("move to " + e.target.id);
     let td2;
-    for(let sy = 0; sy < nombreCases; sy++){
-      for(let sx = 0; sx < nombreCases; sx++){
-        resetCase(document.getElementById('Case' + sx + sy));
-      }
-    }
-    let a = parseInt(e.target.id.charAt(4));
-    let b = parseInt(e.target.id.charAt(5));
+    //la modification de couleurs et d'évènements est retirés des cases et le déplacement du pion est éffectué
+    resetAllCases();
+    
+    //identifiant : "Casexy", récupération de x et y
+    let a = parseInt(e.target.id.charAt(4)); //récupère la coordonnée x
+    let b = parseInt(e.target.id.charAt(5)); //récupère la coordonnée y
+    
+    //déplacement du pion
     pion.move(a,b);
   }
 }
 
+//supprime les évènements et rends les couleurs d'origine à toutes les cases du plateau
+function resetAllCases(){
+  for(let y = 0; y < nombreCases; y++){
+    for(let x = 0; x < nombreCases; x++){
+      resetCase(document.getElementById('Case' + x + y));
+    }
+  }
+}
+
+/*
+* la case donnée en paramètre est réinitialisée
+* suppression des évènements s'ils existent
+* et modification de la couleur pour la couleur d'origine de la case
+*/
 function resetCase(td){
   td.style.color = 'black';
   td.onmouseenter = td.onmouseout = td.onclick = '';
@@ -174,6 +272,6 @@ function resetCase(td){
     td.style.backgroundColor = 'white';
   }
   else {
-    td.style.backgroundColor = 'maroon';
+    td.style.backgroundColor = 'black';
   }
 }
