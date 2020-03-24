5e39dc2bfa497c09700858e55869064f4673d65c
Glitch (chess-uvsq) <none>
2020-03-24 19:46:17 +0000

    🌅🔔 Updated with Glitch commentaires du code

--- public/create/create.js
+++ public/create/create.js
@@ -1,11 +1,10 @@
-var codeSalon;
+var codeSalon = getRandomCode();
 var choix = '';
 
-var socket = io.connect(); //io existe dans create.html, erreur normale
+var socket = io.connect(); //io existe dans create.html, erreur normale si elle apparait
 
 (function main(){
   addEvent();
-  codeSalon = getRandomCode();
   afficheCode();
 })();
 
@@ -18,20 +17,26 @@ function addEvent(){
   let launch = document.getElementById('launch');
   let form = document.getElementById('banniereTable');
   
+  //le formulaire est validé
   form.addEventListener('submit', function(e){
     
+    //l'utilisateur a validé son code de salon personnalisé
     if(choix == 'valider'){
-      if(input.value != '') {
+      if(input.value != '') { // si le code n'est pas vide
+        input.value = formateForCode(input.value); //on supprime les espaces
         codeSalon = input.value;
         afficheCode();
       }
     }
     
+    //choisit un code aléatoire
     if(choix == 'aleatoire'){
       codeSalon = getRandomCode();
       afficheCode();
     }
     
+    //demande au serveur d'ouvrir le salon avec le code indiqué, si le serveur renvoi la reponse 'launched',
+    //alors le salon peut être ouvert et l'utilisateur est renvoyé vers celui-ci
     if(choix == 'launch'){
       form.action = '/salon/' + codeSalon;
       socket.emit('code', codeSalon);
@@ -40,11 +45,12 @@ function addEvent(){
       });
     }
     
+    //renvoi l'utilisateur vers la page d'accueil
     if(choix == 'retourAccueil'){
       form.action = '/'
       form.submit();
     }
-    
+    choix = '';
     e.preventDefault(); //ne pas quitter la page
   });
   
@@ -64,7 +70,22 @@ function getRandomCode() {
   return Math.floor(Math.random() * Math.floor(10000000000)) + 469285;
 }
 
+//affiche le code du salon actuellement choisit par l'utilisateur
 function afficheCode(){
   let code = document.getElementById('afficheCode');
   code.value = '' + codeSalon;
+}
+
+//supprime les espaces dans le code et les remplacent par des '_'
+function formateForCode(code){
+  let split = code.split(' ');
+  code = '';
+  let i;
+  for(i = 0; i < split.length - 1; i++){
+    code += split[i] + '_';
+    console.log(code);
+  }
+  code += split[i];
+  console.log(code);
+  return code;
 }
